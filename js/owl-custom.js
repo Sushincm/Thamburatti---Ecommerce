7b9e0e216d557845a162b6e5d91dfbf2029622e6
Sushincm <sushinofficial@gmail.com>
2025-12-22 00:52:21 +0530

    Discover section added

--- js/owl-custom.js
+++ js/owl-custom.js
@@ -77,4 +77,24 @@ document.addEventListener("DOMContentLoaded", function () {
       $consultCarousel.trigger("prev.owl.carousel");
     });
   });
+
+  // Discpover section
+
+  $(document).ready(function () {
+    $(".sb-discover-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 4500,
+      autoplayHoverPause: true,
+      responsive: {
+        0: { items: 1 }, 
+        576: { items: 2 }, 
+        768: { items: 3 }, 
+        1200: { items: 5 }, 
+      },
+    });
+  });
 });
