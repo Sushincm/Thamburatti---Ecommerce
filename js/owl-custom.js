b8cff65e8467e183cb6154a4c3e942371fd6ecbd
Sushincm <sushinofficial@gmail.com>
2025-12-21 15:53:31 +0530

    top catagory section added

--- js/owl-custom.js
+++ js/owl-custom.js
@@ -1,13 +1,34 @@
-$(document).ready(function () {
-  $(".sb-banner-carousel").owlCarousel({
-    items: 1,
-    loop: true,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    animateOut: "fadeOut",   // fade out effect
-    smartSpeed: 550
+document.addEventListener("DOMContentLoaded", function () {
+  $(document).ready(function () {
+    $(".sb-banner-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      nav: false,
+      dots: false,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      animateOut: "fadeOut", // fade out effect
+      smartSpeed: 550,
+    });
+  });
+
+  // Top categories carousel
+  $(document).ready(function () {
+    $(".sb-topcat-carousel").owlCarousel({
+      loop: true,
+      margin: 24,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 1500,
+      autoplayHoverPause: true,
+      responsive: {
+        0: { items: 2 }, 
+        550: { items: 2 },
+        768: { items: 3 },
+        992: { items: 4 },
+      },
+    });
   });
 });
