260b2be0b9b1a3f246f9fd3f64fa9656915bf14f
Sushincm <sushinofficial@gmail.com>
2026-01-15 20:57:34 +0530

    login page added

--- js/owl-custom.js
+++ js/owl-custom.js
@@ -1,116 +1,104 @@
-document.addEventListener("DOMContentLoaded", function () {
-  $(document).ready(function () {
-    $(".sb-banner-carousel").owlCarousel({
-      items: 1,
-      loop: true,
-      nav: false,
-      dots: false,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true,
-      animateOut: "fadeOut", // fade out effect
-      smartSpeed: 550,
-    });
+$(document).ready(function () {
+  // Banner Carousel
+  $(".sb-banner-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    animateOut: "fadeOut", // fade out effect
+    smartSpeed: 550,
   });
 
   // Top categories carousel
-  $(document).ready(function () {
-    $(".sb-topcat-carousel").owlCarousel({
-      loop: true,
-      margin: 24,
-      nav: true,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 1500,
-      autoplayHoverPause: true,
-      responsive: {
-        0: { items: 2 },
-        550: { items: 2 },
-        768: { items: 3 },
-        992: { items: 4 },
-      },
-    });
+  $(".sb-topcat-carousel").owlCarousel({
+    loop: true,
+    margin: 24,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 2 },
+      550: { items: 2 },
+      768: { items: 3 },
+      992: { items: 4 },
+    },
   });
 
   // New arrivals carousel
-  $(document).ready(function () {
-    $(".sb-arrivals-carousel").owlCarousel({
-      loop: true,
-      margin: 24,
-      nav: true,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 1500,
-      autoplayHoverPause: true,
-      responsive: {
-        0: { items: 1 },
-        576: { items: 2 },
-        992: { items: 3 },
-        1200: { items: 4 },
-      },
-    });
+  $(".sb-arrivals-carousel").owlCarousel({
+    loop: true,
+    margin: 24,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 1 },
+      576: { items: 2 },
+      992: { items: 3 },
+      1200: { items: 4 },
+    },
   });
 
-  //  Consulting section
-  $(document).ready(function () {
-    const $consultCarousel = $(".sb-consulting-carousel");
-
-    $consultCarousel.owlCarousel({
-      loop: true,
-      margin: 24,
-      nav: false,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 1500,
-      autoplayHoverPause: true,
-      responsive: {
-        0: { items: 1 },
-        576: { items: 2 },
-        992: { items: 4 },
-      },
-    });
-
-    $(".sb-consulting__nav--next").on("click", function () {
-      $consultCarousel.trigger("next.owl.carousel");
-    });
-    $(".sb-consulting__nav--prev").on("click", function () {
-      $consultCarousel.trigger("prev.owl.carousel");
-    });
+  // Consulting section
+  const $consultCarousel = $(".sb-consulting-carousel");
+  $consultCarousel.owlCarousel({
+    loop: true,
+    margin: 24,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 1 },
+      576: { items: 2 },
+      992: { items: 4 },
+    },
   });
 
-  // Discpover section
-
-  $(document).ready(function () {
-    $(".sb-discover-carousel").owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: false,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 1500,
-      autoplayHoverPause: true,
-      responsive: {
-        0: { items: 1 },
-        576: { items: 2 },
-        768: { items: 3 },
-        1200: { items: 5 },
-      },
-    });
+  $(".sb-consulting__nav--next").on("click", function () {
+    $consultCarousel.trigger("next.owl.carousel");
+  });
+  $(".sb-consulting__nav--prev").on("click", function () {
+    $consultCarousel.trigger("prev.owl.carousel");
   });
 
-  $(document).ready(function () {
-    $(".sb-reviews-carousel").owlCarousel({
-      loop: true,
-      margin: 24,
-      nav: false,
-      dots: true,
-      autoplay: true,
-      autoplayTimeout: 1500,
-      autoplayHoverPause: true,
-      responsive: {
-        0: { items: 1 },
-        768: { items: 2 },
-      },
-    });
+  // Discover section
+  $(".sb-discover-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 1 },
+      576: { items: 2 },
+      768: { items: 3 },
+      1200: { items: 5 },
+    },
+  });
+
+  // Reviews section
+  $(".sb-reviews-carousel").owlCarousel({
+    loop: true,
+    margin: 24,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplayHoverPause: true,
+    responsive: {
+      0: { items: 1 },
+      768: { items: 2 },
+    },
   });
 });
